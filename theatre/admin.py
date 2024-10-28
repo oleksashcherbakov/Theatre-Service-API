84720d69096731b7a45bc5fd682ab7303362dd49
oleksashcherbakov <oleksa.shcherbakov@gmail.com>
2024-10-28 15:46:11 +0200

    changed on decorator-based approach while registering models

--- theatre/admin.py
+++ theatre/admin.py
@@ -21,9 +21,40 @@ class ReservationAdmin(admin.ModelAdmin):
     inlines = (TicketInline,)
 
 
-admin.site.register(Actor)
-admin.site.register(Genre)
-admin.site.register(Play)
-admin.site.register(TheatreHall)
-admin.site.register(Performance)
-admin.site.register(Ticket)
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name"]
+    list_filter = ["first_name", "last_name"]
+    search_fields = ["last_name"]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "description"]
+    list_filter = ["title"]
+    search_fields = ["title"]
+
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "rows", "seats_in_row"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ["id", "play", "theatre_hall", "show_time"]
+    list_filter = ["play", "theatre_hall", "show_time"]
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ["id", "row", "seat", "performance"]
